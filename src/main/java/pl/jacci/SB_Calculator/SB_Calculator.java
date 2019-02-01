3b25aa71b0cfd5e6c512fc2ec31a5081cc236b6e
Jacci19 <jacek205@wp.pl>
2019-02-01 16:26:00 +0100

    01_initial commit

---
+++ src/main/java/pl/jacci/SB_Calculator/SB_Calculator.java
@@ -0,0 +1,15 @@
+package pl.jacci.SB_Calculator;
+/**
+ * Projekt na podstawie kursu AkademiaKodu: https://www.youtube.com/watch?v=6dldJw2D7i8
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SB_Calculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SB_Calculator.class, args);
+	}
+}
---
+++ src/main/java/pl/jacci/SB_Calculator/models/CalculatorSimple.java
@@ -0,0 +1,28 @@
+package pl.jacci.SB_Calculator.models;
+
+public class CalculatorSimple {
+
+    public int add( int a, int b){
+//        //todo implement method here
+//        return 0;
+        return 0;
+    }
+
+    public int subtract( int a, int b){
+//        //todo implement method here
+//        return 0;
+        return 0;
+    }
+
+    public int multiplication( int a, int b){
+//        //todo implement method here
+//        return 0;
+        return 0;
+    }
+
+    public int division (int a, int b){
+//        //todo implement method here
+//        return 0;
+        return 0;
+    }
+}
